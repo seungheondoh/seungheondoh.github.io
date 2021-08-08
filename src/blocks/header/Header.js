ff1606142da713e96e0db0a63c7d3bbf5a7267b1
SeungHeonDoh <seungheon.doh@gmail.com>
2021-08-09 00:14:07 +0900

    add header

--- src/blocks/header/Header.js
+++ src/blocks/header/Header.js
@@ -16,12 +16,12 @@ const Header = () => {
                                 className="btn btn-link transform-scale-h border-0 p-0"
                                 href="/">Curriculum Vitae</a>
                         </li>
-                        {/* <li className={ "menu-item" + ( window.location.pathname === '/blog' ? " current-menu-item" : "" ) }>
+                        <li className={ "menu-item" + ( window.location.pathname === '/blog' ? " current-menu-item" : "" ) }>
                             <a 
                                 title="blog" 
                                 className="btn btn-link transform-scale-h border-0 p-0"
                                 href="/blog">Blog</a>
-                        </li> */}
+                        </li>
                     </ul>
                 </nav>
             </div>
